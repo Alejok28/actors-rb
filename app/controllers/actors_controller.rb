ab76bf5d2c1d56fd08548a1f9efdec3eb1ae04f2
Alejok28 <alejo_k28@hotmail.com>
2017-12-09 19:18:50 -0500

    Create an actor

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,2 +1,24 @@
 class ActorsController < ApplicationController
+  def index
+    @actors = Actor.all
+  end
+
+  def new
+    @actors = Actor.new
+  end
+
+  def create
+    @actors = Actor.new(actors_params)
+    if @actors.save
+      redirect_to actors_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def actors_params
+    params.require(:actor).permit(:name, :bio, :birth_date, :birth_place, :death_date, :death_place, :image_url, :alive)
+  end
+
 end
